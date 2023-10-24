768e08ad0c48a3f1ed1e63865ff16af1aa12799e
Kazumasa Shimomura <shimomura_kazumasa@cyberagent.co.jp>
2023-10-24 19:29:46 +0900

    feature: resultBuilder for build array

--- Sources/Playbook/FunctionBuilder/ScenariosBuilder.swift
+++ Sources/Playbook/FunctionBuilder/ScenariosBuilder.swift
@@ -1,13 +1,13 @@
 /// The custom parameter attribute that constructs scenarios from multi-statement closures.
-@_functionBuilder
+@resultBuilder
 public struct ScenariosBuilder: ScenariosBuildable {
-    private var _buildScenarios: () -> [Scenario]
+    public var _buildScenarios: () -> [Scenario]
 
     /// Creates a new builder with given closure.
     ///
     /// - Parameters:
     ///   - buildScenarios: A closure that to build an array of scenarios.
-    private init(_ buildScenarios: @escaping () -> [Scenario]) {
+    public init(_ buildScenarios: @escaping () -> [Scenario]) {
         self._buildScenarios = buildScenarios
     }
 
@@ -20,6 +20,10 @@ public struct ScenariosBuilder: ScenariosBuildable {
     /// - Parameters:
     ///   - scenarios: A set of scenarios to be constructed.
     public static func buildBlock(_ scenarios: ScenariosBuildable...) -> ScenariosBuilder {
+        buildArray(scenarios)
+    }
+
+    public static func buildArray(_ scenarios: [ScenariosBuildable]) -> ScenariosBuilder {
         ScenariosBuilder {
             scenarios.flatMap { $0.buildScenarios() }
         }
